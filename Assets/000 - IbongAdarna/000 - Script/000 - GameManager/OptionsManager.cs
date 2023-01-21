64b2b07485f8af42095591346b0eac483f32ee92
Jan Bien Gabrielle A. Daniel <daniel.janbiengabrielle@gmail.com>
2023-01-21 23:10:57 +0800

    update

--- Assets/000 - IbongAdarna/000 - Script/000 - GameManager/OptionsManager.cs
+++ Assets/000 - IbongAdarna/000 - Script/000 - GameManager/OptionsManager.cs
@@ -1,3 +1,4 @@
+using MyBox;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,29 @@ using UnityEngine.UI;
 public class OptionsManager : MonoBehaviour
 {
     [SerializeField] private Slider volumeSlider;
+    [SerializeField] private bool gameplay;
+    [ConditionalField("gameplay")] [SerializeField] private GameObject settingsObj;
+
+    private void Awake()
+    {
+        if (gameplay) volumeSlider.value = GameManager.Instance.SoundMnger.CurrentVolume;
+    }
 
     public void ChangeVolume()
     {
         GameManager.Instance.SoundMnger.CurrentVolume = volumeSlider.value;
+        PlayerPrefs.SetFloat("volumeData", GameManager.Instance.SoundMnger.CurrentVolume);
     }
 
     public void CheckVolume()
     {
         volumeSlider.value = GameManager.Instance.SoundMnger.CurrentVolume;
+    }
+
+    public void SettingsEnabler(bool value)
+    {
+        if (!gameplay) return;
+
+        settingsObj.SetActive(value);
     }
 }
